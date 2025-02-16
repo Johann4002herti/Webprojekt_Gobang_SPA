eeed07781ca6e4b7564b998e13485a0ef6151340
Johann Hertrampf <Johann.Hertrampf@stud.hszg.de>
2025-02-16 19:31:05 +0100

    fixed Error

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist/spa'))); // Passe den Pfad zum dist-Ordner an
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/spa/index.html')); // Passe den Pfad an
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Server is running');
+});
