f6b8c7fda461fe45b47d07cf3bbfa313a3c1150b
Johann Hertrampf <Johann.Hertrampf@stud.hszg.de>
2025-02-17 10:26:43 +0100

    corrected Path in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));//,'spa'))); // Passe den Pfad zum dist-Ordner an
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist','spa','index.html')); // Passe den Pfad an
+  res.sendFile(path.join(__dirname, 'dist','spa','browser','index.html')); // Passe den Pfad an
 });
 
 app.listen(process.env.PORT || 8080, () => {
