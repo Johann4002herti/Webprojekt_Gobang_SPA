cab6dad3ad9fb24987c22c5a2f8dfae5ad151cb6
Johann Hertrampf <Johann.Hertrampf@stud.hszg.de>
2025-02-17 09:36:08 +0100

    Change ng with npm in package.json

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -10,45 +10,51 @@ import $ from 'jquery';
 })
 export class GameComponent {
   gameComponent : GameComponent = this;
-  gameStatus : string = "isRunning";
+  static gameStatus : string = "isRunning";
   playersTurn : string = "One";
   game : any;
-  gameCode : string  = "";
+  static gameCode : string  = "";
   localPlayer : string = "";
-  static blackPiece :string = "assets/Gobang/blackPiece_1";
-  static whitePiece :string = "assets/Gobang/whitePiece_1";
+  blackPiece :string;
+  whitePiece :string;
   background : string;
 
   constructor() {
     this.background   = "assets/Gobang/background_1.png"
+    this.blackPiece   = "assets/Gobang/blackPiece_1"
+    this.whitePiece   = "assets/Gobang/whitePiece_1"
   }
 
   async ngOnInit(){
     let temp = localStorage.getItem('gameCode');
     if (temp != null){
-      this.gameCode = temp
+      GameComponent.gameCode = temp
     }
     temp = localStorage.getItem("localPlayer");
     if (temp != null){
       this.localPlayer = temp
     }
     await new Promise(f => setTimeout(f, 20));
-    this.getBoard()
-    setInterval(this.getBoard,5000);
+    while (false){
+      await new Promise(f => setTimeout(f, 2000));
+      GameComponent.getBoard()
+    }
+    //setInterval(this.getBoard(),5000);
   }
 
     makeMove(i : number,j : number) {
       console.log("called makeMove with: "+i+","+j);
       if(this.playersTurn == this.localPlayer) {
         $.ajax({
-          url: 'https://tranquil-ravine-52782-34bf80f0f653.herokuapp.com/api/game/board/tiles?TileX='+i+'&TileY='+j+'&gameCode='+this.gameCode,
+          url: 'https://tranquil-ravine-52782-34bf80f0f653.herokuapp.com/api/game/board/tiles?TileX='+i+'&TileY='+j+'&gameCode='+GameComponent.gameCode,
           type: 'put',
           dataType: 'json',
           processData: false,
           contentType: "application/json; charset=UTF-8",
           success: function (data) {
             console.log(data.Message)
-            this.getBoard()
+
+              GameComponent.getBoard()
           },
           error: function (xhr, ajaxOptions, thrownError) {
             alert('Error: ' + xhr.status + '   ' + thrownError);
@@ -57,7 +63,7 @@ export class GameComponent {
       }
     }
 
-    async getBoard() {
+    static async getBoard() {
       await new Promise(f => setTimeout(f, 20));
     console.log("called GetBoard")
       console.log(this.gameStatus)
@@ -83,18 +89,18 @@ export class GameComponent {
 
             let DS = "<table class='tableBoard'>";
             for (let i = 0; i < data.Board.Size; i++) {
-              DS += "<tr class='tableMarginPadding'>";
+              DS += "<tr>";
               for (let j = 0; j < data.Board.Size; j++) {
-                DS += "<td class='tableMarginPadding'>" +
+                DS += "<td class='tableMarginPadding' >" +
                   //"<span class='field' id='" + i + "_" + j + "' (click)='makeMove("+i+","+j+")'>hallo</span>"+
-                  "<img src = 'assets/Gobang/background_1.png' class='field' id='" + i + "_" + j + "' alt='image' (click)='makeMove("+i+","+j+")'>" +
+                  "<img src='assets/Gobang/background_1.png' class='field' id='" + i + "_" + j + "' alt='image' (click)='makeMove("+i+","+j+")' width='30px' height='30px'>" +
                   "</td>";
               }
               DS += "</tr>";
             }
             DS += "</table>";
 
-            console.log(DS)
+            //console.log(DS)
             $("#board").html(DS);
 
             //
@@ -127,11 +133,11 @@ export class GameComponent {
     static checkStatus(i : number,j : number, status : string) {
       console.log("it works")
       if(status == "playerOne"){
-        $("#"+i+"_"+j).attr("src", GameComponent.blackPiece);
+        $("#"+i+"_"+j).attr("src","assets/Gobang/blackPiece_1.png");
       } else if (status == "playerTwo") {
-        $("#"+i+"_"+j).attr("src",GameComponent.whitePiece);
+        $("#"+i+"_"+j).attr("src","assets/Gobang/whitePiece_1.png");
       } else {
-        //$("#"+i+"_"+j).attr("src", "background");
+        $("#"+i+"_"+j).attr("src", "assets/Gobang/background_1.png");
       }
     }
 
